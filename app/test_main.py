3ad93fe6acb509b3fca8c1be31eb3c15e7b6c1dd
Pavitra <pavitra04singh@gmail.com>
2025-09-28 20:42:54 +0530

    updated message

--- app/test_main.py
+++ app/test_main.py
@@ -22,7 +22,7 @@ def test_greet_multiple():
 def test_greet_special_char():
     response = client.get("/greet/?name=Abc@123")
     assert response.status_code == 200
-    assert response.json() == {"message": "Invalid name. Special characters are not allowed."}
+    assert response.json() == {"message":"Invalid name. Special characters are not allowed."}
 
 
 def test_greet_multiple_param():
